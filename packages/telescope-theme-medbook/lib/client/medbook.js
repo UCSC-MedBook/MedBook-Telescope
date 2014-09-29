12818e03b791e38324e76651f578694e4083da74
Ted Goldstein <ted@soe.ucsc.edu>
2014-09-29 12:53:09 -0700

    Start at integrating Robert's portal.

--- packages/telescope-theme-medbook/lib/client/medbook.js
+++ packages/telescope-theme-medbook/lib/client/medbook.js
@@ -2,5 +2,5 @@ templates["posts_list"] = "MedBookMainPage";
 
 // use this instead of Jquery ready() function.
 Template[getTemplate('posts_list')].rendered = function(){
-    $(document).ready(function() { $( "#tabs" ).tabs(); });
+    $(document).ready(function() { $( ".tabs" ).tabs(); });
 }
