714962b4c37a1ba93e296fce9d53bff98a5bc9a9
Robert Baertsch <baertsch@Roberts-MacBook-Pro.local>
2014-09-29 10:10:09 -0700

    medbook portal

---
+++ client/MedBook.js
@@ -0,0 +1,38 @@
+SampleGroups = new Mongo.Collection("sampleGroup");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+  Session.setDefault("selectedDatasetId", "G3azHfDnMhXm9oAaZ");
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get("counter");
+    }
+  });
+
+  Template.start.events({
+    'click button.scv': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set("counter", Session.get("counter") + 1);
+    }
+  });
+  Template.updateDatasetsForm.helpers({
+    editingDataset: function editingDatasetHelper() {
+      return Datasets.findOne({_id: Session.get("selectedDatasetId")});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
