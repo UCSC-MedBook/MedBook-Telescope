f36e28076f834aec5e87441e299e0d21f4a871e3
Ted Goldstein <ted@soe.ucsc.edu>
2014-09-29 02:07:12 -0700

    Cleanup.

--- packages/telescope-medbook/lib/client/medbook.js
+++
@@ -1,6 +0,0 @@
-templates["posts_list"] = "MedBookMainPage";
-
-// use this instead of Jquery ready() function.
-Template[getTemplate('posts_list')].rendered = function(){
-    $(document).ready(function() { $( "#tabs" ).tabs(); });
-}
--- packages/telescope-medbook/lib/client/views/post_crfs.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(function () {
-  Template[getTemplate('postCRFs')].helpers({
-    crfLink: function(){
-      return getCRFUrl(this.slug);
-    }
-  });
-});
